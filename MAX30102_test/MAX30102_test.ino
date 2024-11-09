885b86dff6cdc401a3b39c660e3de5157b3e99aa
Waklink <catalin.dinu@edu.uah.es>
2024-11-09 13:40:32 +0100

    Max30102 calibrado lo mejor que he podido
    .

--- MAX30102_test/MAX30102_test.ino
+++ MAX30102_test/MAX30102_test.ino
@@ -1,9 +1,11 @@
 #include <Wire.h>
 #include "MAX30105.h"
 #include "spo2_algorithm.h"
+#include "heartRate.h"
 
 MAX30105 particleSensor;
 
+// Variables for spo2
 uint32_t irBuffer[100]; //infrared LED sensor data
 uint32_t redBuffer[100];  //red LED sensor data
 int32_t bufferLength = 100; //data length
@@ -11,6 +13,14 @@ int32_t spo2; //SPO2 value
 int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
 int32_t heartRate; //heart rate value
 int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+// Variable for heart rate
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
 
 void setup()
 {
@@ -22,11 +32,11 @@ void setup()
     while (1);
   }
 
-  byte ledBrightness = 45; //Options: 0=Off to 255=50mA
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  byte sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 118; //Options: 69, 118, 215, 411
   int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
 
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
@@ -43,6 +53,25 @@ void loop()
     redBuffer[i] = particleSensor.getRed();
     irBuffer[i] = particleSensor.getIR();
     particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    if (checkForBeat(irBuffer[i]) == true) {
+      //We sensed a beat!
+      long delta = millis() - lastBeat;
+      lastBeat = millis();
+
+      beatsPerMinute = 60 / (delta / 1000.0);
+
+      if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+        rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+        rateSpot %= RATE_SIZE; //Wrap variable
+
+        //Take average of readings
+        beatAvg = 0;
+        for (byte x = 0 ; x < RATE_SIZE ; x++)
+          beatAvg += rates[x];
+        beatAvg /= RATE_SIZE;
+      }
+    }
   }
 
   //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
@@ -67,28 +96,65 @@ void loop()
       redBuffer[i] = particleSensor.getRed();
       irBuffer[i] = particleSensor.getIR();
       particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      if (checkForBeat(irBuffer[i]) == true) {
+        //We sensed a beat!
+        long delta = millis() - lastBeat;
+        lastBeat = millis();
+
+        beatsPerMinute = 60 / (delta / 1000.0);
+
+        if (beatsPerMinute < 255 && beatsPerMinute > 20) {
+          rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+          rateSpot %= RATE_SIZE; //Wrap variable
+
+          //Take average of readings
+          beatAvg = 0;
+          for (byte x = 0 ; x < RATE_SIZE ; x++)
+            beatAvg += rates[x];
+          beatAvg /= RATE_SIZE;
+        }
+      }
+
+      // For calibrating
+      // Serial.print(F("red="));
+      // Serial.print(redBuffer[i], DEC);
+      // Serial.print(F(", ir="));
+      // Serial.print(irBuffer[i], DEC);
+      // Serial.print(", BPM=");
+      // Serial.print(beatsPerMinute);
+      // Serial.print(", Avg BPM=");
+      // Serial.print(beatAvg);
+      // if (validHeartRate) {
+      //   Serial.print(" Pulse: ");
+      //   Serial.print(heartRate);
+      //   Serial.print(" BPM, ");
+      // } else {
+      //   Serial.print(" Pulse not valid, ");
+      // }
+      
+      // if (validSPO2) {
+      //   Serial.print("SpO2: ");
+      //   Serial.print(spo2);
+      //   Serial.println(" %");
+      // } else {
+      //   Serial.println("SpO2 not valid");
+      // }
     }
 
     //After gathering 25 new samples recalculate HR and SP02
     maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
-    // Print the results if they are valid
-    if (validHeartRate) {
-      Serial.print(" Pulse: ");
-      Serial.print(heartRate);
-      Serial.print(" BPM, ");
-    } else {
-      Serial.print(" Pulse not valid, ");
-    }
-    
+    // Print the results
+    Serial.print("Pulse: ");
+    Serial.print(beatAvg);
+    Serial.print(" BPM");
     if (validSPO2) {
-      Serial.print("SpO2: ");
+      Serial.print(", SpO2: ");
       Serial.print(spo2);
       Serial.println(" %");
     } else {
-      Serial.println("SpO2 not valid");
+      Serial.println(", SpO2 not valid");
     }
-
-    delay(1000);
   }
 }
