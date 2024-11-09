89b39428e4ab08dbdbfabf723195c19b61a9db1e
Waklink <catalin.dinu@edu.uah.es>
2024-11-09 12:52:46 +0100

    Update MAX30102_test.ino

--- MAX30102_test/MAX30102_test.ino
+++ MAX30102_test/MAX30102_test.ino
@@ -4,68 +4,91 @@
 
 MAX30105 particleSensor;
 
-// Variables for SpO2 and pulse
-uint32_t irBuffer[100];  // Save IR samples
-uint32_t redBuffer[100]; // Save the red samples
-int32_t bufferLength = 100; // Buffer length
-int32_t spo2; // Blood oxygen saturation level
-int8_t validSPO2; // Validity of the SpO2 measurement (0 invalid, 1 valid)
-int32_t heartRate; // Heart rate
-int8_t validHeartRate; // Validity of the heart rate measurement (0 invalid, 1 valid)
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+int32_t bufferLength = 100; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
 
-void setup() {
-  Serial.begin(115200);
-  // Initialise the sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) { 
-    Serial.println("Sensor MAX30102 was not found. Connect the sensor and reboot.");
+void setup()
+{
+  Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
   }
-  
-  Serial.println("Sensor MAX30102 initialised.");
-  
-  // Configure the sensor
-  byte ledBrightness = 31; //Options: 0=Off to 255=50mA
+
+  byte ledBrightness = 45; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
   byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-  byte sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
   int pulseWidth = 411; //Options: 69, 118, 215, 411
   int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); // Settings to optimise SpO2 and pulse
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
 }
 
-void loop() {
-  // Fills the buffer with the initial data
-  for (int i = 0; i < bufferLength; i++) {
-    while (particleSensor.available() == false) { // Wait for data to become available
-      particleSensor.check(); // Reads sensor data
-    }
-    
-    // Stores the data in the corresponding buffers
+void loop()
+{
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
     redBuffer[i] = particleSensor.getRed();
     irBuffer[i] = particleSensor.getIR();
-    
-    particleSensor.nextSample(); // Go to the next data
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
   }
 
-  // Calls the SpO2 and pulse algorithm
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
-  // Print the results if they are valid
-  if (validHeartRate) {
-    Serial.print("Pulse: ");
-    Serial.print(heartRate);
-    Serial.print(" BPM, ");
-  } else {
-    Serial.print("Pulse not valid, ");
-  }
-  
-  if (validSPO2) {
-    Serial.print("SpO2: ");
-    Serial.print(spo2);
-    Serial.println(" %");
-  } else {
-    Serial.println("SpO2 not valid");
-  }
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
 
-  delay(1000); // Time interval for the next measurement
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    }
+
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+    // Print the results if they are valid
+    if (validHeartRate) {
+      Serial.print(" Pulse: ");
+      Serial.print(heartRate);
+      Serial.print(" BPM, ");
+    } else {
+      Serial.print(" Pulse not valid, ");
+    }
+    
+    if (validSPO2) {
+      Serial.print("SpO2: ");
+      Serial.print(spo2);
+      Serial.println(" %");
+    } else {
+      Serial.println("SpO2 not valid");
+    }
+
+    delay(1000);
+  }
 }
